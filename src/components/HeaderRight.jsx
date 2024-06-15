c8edf2529f1c7eba5776e703887dfb17539e28bf
Sam Ragucci <samr742@gmail.com>
2024-06-14 19:11:38 -0700

    cleanup

--- src/components/HeaderRight.jsx
+++ src/components/HeaderRight.jsx
@@ -5,15 +5,9 @@ import { usePathname } from 'next/navigation';
 import React from 'react'
 
 const NavItem = ({href, children}) => {
-  // let isActive = usePathname() === href;
   return (
     <li>
-      <Link href={href} className={clsx('relative block px-3 py-2 transition'
-      // , 
-      //     isActive ? 
-      //     'text-teal-500 dark:text-teal-400' : 
-      //     'hover:text-teal-500 dark:hover:*:text-teal-400'
-          )}>
+      <Link href={href} className={clsx('relative block px-3 py-2 transition')}>
         {children}
       </Link>
     </li>
@@ -37,9 +31,7 @@ const NavItem = ({href, children}) => {
 const DesktopNavigation = (props) => {
   return (
     <nav {...props}>
-      <ul className='flex rounded-md bg-white/90 px-3 text-sm font-medium text-zinc-800 
-      shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur
-      dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10'>
+      <ul className='flex rounded-md bg-white/90 px-3 text-sm font-medium'>
       <NavItem href={'/about'}>About</NavItem>
       <NavItem href={'/contact'}>Contact</NavItem>
       </ul>
