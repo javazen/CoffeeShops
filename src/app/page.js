07536c237e73d2732d9c91aff1f38b64851f9509
Sam Ragucci <samr742@gmail.com>
2024-06-17 16:42:51 -0700

    add FeaturedPosts stub

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,11 @@
+import FeaturedPosts from "@/components/FeaturedPosts";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <h1 className="text-5xl font-bold">
-          Coming soon
-        </h1>
+        <FeaturedPosts />
       </div>
     </main>
   );
---
+++ src/components/FeaturedPosts.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FeaturedPosts = ({posts}) => {
+  return (
+    <div>FeaturedPosts</div>
+  )
+}
+
+export default FeaturedPosts
