a79170b04b0d30209f0b8a2de2b5b7e0886436cb
Sam Ragucci <samr742@gmail.com>
2024-06-20 10:12:19 -0700

    getAllPosts, importPost
    no sorting yet

---
+++ src/lib/post.js
@@ -0,0 +1,18 @@
+import { glob } from "fast-glob";
+
+export async function getAllPosts() {
+    let postFilenames = await glob("*/page.mdx", {
+        cwd: "./src/app/posts",
+    });
+    let posts = await Promise.all(postFilenames.map(importPost));
+    return posts;
+    // return articles.sort((a, b) => new Date(b.date).valueOf() - new Date(a.date).valueOf());
+}
+
+export async function importPost(postFilename) {
+    let {post} = (await import(`../app/posts/${postFilename}`));
+    return {
+        slug: postFilename.replace(/(\/page)?\.mdx$/, ""),
+        ...post
+    }
+}
