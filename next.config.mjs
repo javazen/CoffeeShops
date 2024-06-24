5bc7ea7c3b10f83124c94bce856d55cf986b2fc8
Sam Ragucci <samr742@gmail.com>
2024-06-24 10:49:51 -0700

    add remarkGfm plugin

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,20 @@
-import nextMDX from "@next/mdx";
+import createMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     pageExtensions: ["js", "jsx", "mdx"],
   };
 
-const withMDX = nextMDX({
-    extension: /\.mdx?$/,
-});
+// const withMDX = nextMDX({
+//     extension: /\.mdx?$/,
+// });
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
+})
   
 export default withMDX(nextConfig);
