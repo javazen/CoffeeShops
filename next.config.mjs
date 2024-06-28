0f062521f96080eaf65c224aefbb9a6ccbecb847
Sam Ragucci <samr742@gmail.com>
2024-06-28 10:03:24 -0700

    try nextMDX, not createMDX

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,42 @@
-import createMDX from "@next/mdx";
-import remarkGfm from "remark-gfm";
+import nextMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
+// import remarkParse from 'remark-parse'
+// import remarkRehype from 'remark-rehype'
+// import rehypeStringify from 'rehype-stringify'
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    pageExtensions: ["js", "jsx", "mdx"],
-  };
-
-// const withMDX = nextMDX({
-//     extension: /\.mdx?$/,
-// });
-const withMDX = createMDX({
-  // Add markdown plugins here, as desired
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
   options: {
     remarkPlugins: [remarkGfm],
     rehypePlugins: [],
+    // remarkPlugins: [remarkGfm, remarkParse, remarkRehype],
+    // rehypePlugins: [rehypeStringify],
   },
 })
+
+// Define the pageExtensions option to have Next handle .mdx files in the pages directory as pages:
+export default withMDX({
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+})
+
+
+
+// import createMDX from "@next/mdx";
+// import remarkGfm from "remark-gfm";
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     pageExtensions: ["js", "jsx", "mdx"],
+//   };
+
+// const withMDX = createMDX({
+//   // Add markdown plugins here, as desired
+//   options: {
+//     remarkPlugins: [remarkGfm],
+//     rehypePlugins: [],
+//   },
+// })
   
-export default withMDX(nextConfig);
+// export default withMDX(nextConfig);
+
+
