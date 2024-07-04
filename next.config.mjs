023fddf576268a58dd16ef62da4e46148468df85
Sam Ragucci <samr742@gmail.com>
2024-07-04 11:35:55 -0700

    comment out plugins

--- next.config.mjs
+++ next.config.mjs
@@ -18,25 +18,3 @@ const withMDX = nextMDX({
 export default withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
 })
-
-
-
-// import createMDX from "@next/mdx";
-// import remarkGfm from "remark-gfm";
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     pageExtensions: ["js", "jsx", "mdx"],
-//   };
-
-// const withMDX = createMDX({
-//   // Add markdown plugins here, as desired
-//   options: {
-//     remarkPlugins: [remarkGfm],
-//     rehypePlugins: [],
-//   },
-// })
-  
-// export default withMDX(nextConfig);
-
-
